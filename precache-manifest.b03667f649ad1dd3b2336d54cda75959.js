054762190025d62d9fc234b0bfdca15777ba3b70
walter708 <uchewalter8@gmail.com>
2020-05-24 16:09:44 -0400

    Updates

---
+++ precache-manifest.b03667f649ad1dd3b2336d54cda75959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccccf6205e2c35f2b634461189c9fb6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "51dde422378f80c2f9de",
+    "url": "/monsters-rolodex/static/css/main.348174d9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51dde422378f80c2f9de",
+    "url": "/monsters-rolodex/static/js/main.636ab64a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
